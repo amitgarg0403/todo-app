198792b9f4a713a43a321032354d7ec8e1cdefe3
amitgarg0403 <amitgarg0403@gmail.com>
2023-10-04 21:33:56 +0530

    todoApp complete till item read and display

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # todo-app
+Created this App using HTML, CSS (frontend part) and Node, Express, MongoDB (Backend)
+
+
+About
+Welcome to Todo App, your ultimate companion for staying organized, boosting productivity, and achieving your goals. 
+
+Key Features
+Simple Material like UI-Design
+
+future Scope
+Will add more functionality to create multiple Lists, changing color, add media etc
---
+++ server.js
@@ -0,0 +1,61 @@
+// requiring all modules and packages here
+const bodyParser = require("body-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Middlewares
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}))
+app.set('view engine', 'ejs')
+
+
+// connect to mongodb
+// this url if we want to connect to monogodb cluster URL= "mongodb+srv://new-test:<password>>@todolist.bj1hus3.mongodb.net/?retryWrites=true&w=majority"
+URL1="mongodb://127.0.0.1:27017/toDoListDB"
+mongoose.connect(URL1)
+.then(()=> (console.log("connected to MongoDB")))
+.catch((err)=>( console.log(err)))
+
+
+// mongoose schema
+const listSchema = new mongoose.Schema ({
+    name:String
+})
+
+// Mongoose model
+const Item = mongoose.model("item", listSchema);
+
+
+// adding Public folder for css and img files
+app.use(express.static("public"))
+
+
+// get request for homeroute
+app.get("/", function(req,res){
+    Item.find()
+    .then(foundItems=>{
+        res.render("list", {title:"ToDo-App", newList: foundItems})
+    })
+    .catch(err=>{(console.log(err))})   
+})
+    
+
+// post method for reading input items
+app.post("/", function(req,res){
+    const item = new Item({
+        name: req.body.newItem
+    })
+    item.save()
+    .then(result => (
+        console.log("New Item Saved")
+    ))
+    .catch((err)=> console.log(err))
+    res.redirect("/");
+})
+
+
+    
+// listenig at Port 
+app.listen("3000", ()=>console.log("Server started at Port 3000"))
+
+
